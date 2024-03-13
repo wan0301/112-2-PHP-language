39dc83a47b694a52374dd897f4366850fed641e1
wan <125788956+wan0301@users.noreply.github.com>
2024-03-13 23:30:31 +0800

    Add files via upload

---
+++ week3.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>個人履歷表</title>   
+
+</head>
+<body bgcolor="#467500">
+<center> <font size ="30", color = "black"><b>個人履歷表</b></font></center>
+<hr/>
+<img src="履歷表照片.jpg" width="400" height="300" alt="履歷表照片應該出現的...">
+<h1>簡介</h1>
+<ul>
+    <li><b>姓名</b>：萬柏彥</li>
+    <li><b>校系</b>：<a href="https://www.nuk.edu.tw/">國立高雄大學</a> 資訊管理學系</li>
+    <li><b>興趣</b>：跑步、攝影、爬山</li>
+    <li><b>個性</b>：隨和、樂觀</li>
+</ul>
+<h1>自我介紹</h1>
+<p>我畢業於國立高雄大學資訊管理學系，在大學時對前端網頁設計非常有興趣，因此XXXXXXXXXX</p>
+<h1>個人資料</h1>
+<ul>
+    <li><b>聯絡電話</b>：萬柏彥</li>
+    <li><b>電子信箱</b>：a1113363@mail.nuk.edu.tw</li>
+    <li><b>通訊地址</b>：高雄市楠梓區XXXXXX</li>
+</ul>
+
+<table border="1">
+<tr>
+    <th>年分</th>
+    <th>畢業學校</th>
+</tr>
+<tr>
+    <th>105</th>
+    <th>台南市立永康國小</th>
+</tr>
+<tr>
+    <th>108</th>
+    <th>台南市立永康國中</th>
+</tr>
+<tr>
+    <th>111</th>
+    <th>國立台南第二高級中學</th>
+</tr>
+<tr>
+    <th>115</th>
+    <th>國立高雄大學 資訊管理學系</th>
+</tr>
+<table>
+
+</body>
+</html>
